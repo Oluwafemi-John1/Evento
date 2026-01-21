cdb69861617b5d4d253f85eb0f6e75a96b792265
Oyeniran Oluwafemi John <oyeniranoluwafemi36@gmail.com>
2026-01-21 17:27:56 +0100

    feat: integrate service worker registration for PWA updates

--- client/src/main.js
+++ client/src/main.js
@@ -1,6 +1,15 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import { useRegisterSW } from 'virtual:pwa-register/vue'
+
+const { updateServiceWorker } = useRegisterSW({
+  onNeedRefresh() {
+    if (confirm('New version available. Reload?')) {
+      updateServiceWorker()
+    }
+  }
+})
 
 const app = createApp(App)
 
